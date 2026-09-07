35fd1bbf7ad0bad3fa40d857c6e5d41604b9d397
Shashwath <shashwath.manjunath@gmail.com>
2026-09-06 18:34:23 -0700

    Update Homebrew formula with tag and sha

--- Formula/songviz.rb
+++ Formula/songviz.rb
@@ -1,8 +1,8 @@
 class Songviz < Formula
   desc "Turn any song into a bar-spectrum visualizer video"
-  homepage "https://github.com/YOURUSERNAME/songviz"
-  url "https://github.com/YOURUSERNAME/songviz/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_SHASUM_AFTER_TAGGING"
+  homepage "https://github.com/sasobhabha/songviz"
+  url "https://github.com/sasobhabha/songviz/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "31dcb20730e85144ed18cb215dbac198391eaf44f64eedf910a46d172c608223"
   license "MIT"
 
   depends_on "ffmpeg"
